ea6d1c449ab27dcf2fa9b1c3d399d6cd90b56f9f
unknown <devlead@dynamisdevelopment.com>
2019-06-15 19:44:47 -0400

    Restarted Course

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -2,16 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour {
+public class helloWorld : MonoBehaviour
+{
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
 
-	// Use this for initialization
-	void Start () {
-		int speed = 50;
-		Debug.Log("Your speed is " + speed);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        int hitpoints = 20;
+        Debug.Log(hitpoints);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Up
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up arrow");
+            min = guess;
+        }
+        // Down
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow");
+            max = guess;
+        }
+        // Enter
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter");
+            guess = 700;
+        }
+
+    }
 }
