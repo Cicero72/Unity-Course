cccb14d152f69a61164fd17592e3e688b5de0dc5
unknown <devlead@dynamisdevelopment.com>
2019-06-17 12:39:40 -0400

    Completed Basic Structure

---
+++ Assets/scripts/AdventureGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    // Use this for initialization
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Space) && nextStates.Length == 1)
+        {
+            state = nextStates[0];
+        }
+        else if (nextStates.Length > 1)
+        {
+            for (int i = 0; i < nextStates.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i)) state = nextStates[i];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
